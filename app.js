b48430ad8b135db183ab38b458da373271bbfe7d
Eihab <mastereihab@gmail.com>
2020-08-20 16:07:38 +0100

    added single meal display

--- app.js
+++ app.js
@@ -18,8 +18,7 @@ function searchMeal(e) {
     if (term.trim()) {
         fetch(`${API_URL}?s=${term}`)
             .then(res => res.json())
-                .then(data => {
-                    console.log(data)
+                .then(data => {                    
                     resultHeading.innerHTML = `
                         <h2>Search result for "${term}":</h2>
                     `
@@ -41,10 +40,60 @@ function searchMeal(e) {
                     }
                 });
                 searchInput.value= "";
-    } else {
-
     }
+}
+
+function getMealByID(id) {
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
+        .then(res => res.json())
+            .then(data =>{
+                const meal = data.meals[0]
+                addMealToDOM(meal)
+            })
+
+}
+
+function addMealToDOM(meal) {
+    let ingredients = [];
+    for (let i = 1; i <= 20; i++) {
+        if(meal[`strIngredient${i}`]) {
+            ingredients.push(`${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`)
+        } else {
+            break;
+        }
+    }
+    singleMealEl.innerHTML = `
+        <div class="single-meal">
+            <h1>${meal.strMeal}</h1>
+            <img src="${meal.strMealThumb}" alt="${meal.strMeal}"/>
+            <div class="single-meal-info">
+                ${meal.strCategory ? `<p>${meal.strCategory}</p>` : ""}
+                ${meal.strArea ? `<p>${meal.strArea}</p>` : ""}
+            </div>
+            <div class="main">
+                <p>${meal.strInstructions}</p>
+                <h2>Ingredients:</h2>
+                <ul>
+                    ${ingredients.map(ingredient => `<li>${ingredient}</li>`).join("")}
+                </ul>
+            </div>
+        </div>
+    `
 }
 
 // Event Listeners
 submit.addEventListener("submit", searchMeal);
+mealsEl.addEventListener("click", e => {
+    const mealInfo = e.path.find(item => {
+        if (item.classList) {
+            return item.classList.contains("meal-info")
+        } else {
+            return false;
+        }
+    });
+
+    if (mealInfo) {
+        const mealID = mealInfo.getAttribute("data-mealid");
+        getMealByID(mealID)
+    }
+});
